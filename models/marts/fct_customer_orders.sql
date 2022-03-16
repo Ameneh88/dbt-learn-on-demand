48543d22783033f79c496569ca6ae285de674887
Ameneh Arabi <amene.javaneh@gmail.com>
2022-03-16 20:23:56 +0000

    pulled out another subquery and replaced with CTE

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -20,7 +20,7 @@ payments as (
         {{ source('dbt-learning-project-343515','payments') }} 
 ),
 
-p as (
+completed_payments as (
     select 
             orderid as order_id, 
             max(created) as payment_finalized_date, 
@@ -37,22 +37,33 @@ paid_orders as (
         orders.user_id    as customer_id,
         orders.order_date as order_placed_at,
         orders.status as order_status,
-        p.total_amount_paid,
-        p.payment_finalized_date,
+        completed_payments.total_amount_paid,
+        completed_payments.payment_finalized_date,
         c.first_name    as customer_first_name,
         c.last_name as customer_last_name
     from 
          orders
-    left join p on orders.id = p.order_id
+    left join completed_payments on orders.id = completed_payments.order_id
     left join customers as c on orders.user_id = c.id 
     ),
- 
+
+cumulative_payments as (
+    select
+        p.order_id,
+        sum(t2.total_amount_paid) as clv_bad
+    from 
+        paid_orders p
+    left join paid_orders t2 on p.customer_id = t2.customer_id 
+    and p.order_id >= t2.order_id
+    group by 1
+    order by p.order_id
+),
 customer_orders as (
     select 
         c.id as customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(orders.id) as number_of_orders,
+        count(orders.id) as number_of_orders
     from 
         customers as c 
     left join orders on orders.user_id = c.id 
@@ -67,21 +78,10 @@ customer_orders as (
         then 'new'
         else 'return' 
         end as nvsr,
-        x.clv_bad as customer_lifetime_value,
+        cp.clv_bad as customer_lifetime_value,
         c.first_order_date as fdos
     from paid_orders p
     left join customer_orders as c using (customer_id)
-    left outer join 
-    (
-        select
-            p.order_id,
-            sum(t2.total_amount_paid) as clv_bad
-        from 
-            paid_orders p
-        left join paid_orders t2 on p.customer_id = t2.customer_id 
-        and p.order_id >= t2.order_id
-        group by 1
-        order by p.order_id
-    ) x on x.order_id = p.order_id
+    left outer join cumulative_payments as cp on cp.order_id = p.order_id
     order by order_id
 
